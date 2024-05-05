be5d1dd72a72308edb780c9c08b55d7804e6da20
Viktoriia Kratser <poshta.victoria@gmail.com>
2024-05-05 23:53:21 +0200

    [tier-01] Task/01 Created variables: name and age.

---
+++ module-01/01-variables.py
@@ -0,0 +1,11 @@
+# Create a variable name and assign it your name, for example, 'Viktoriia'. 
+# Create a variable age and assign it your age.
+
+# Substitute 'Viktoriia' with your actual name
+name = "Viktoriia"
+
+# Substitute 34 with your actual age
+age = 34
+
+# Print the result.
+print(f"Your name is: {name}. You are {age} years old.")
