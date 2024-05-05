19aabe80708080b4618155f0c7e2d02a353e58ff
Viktoriia Kratser <poshta.victoria@gmail.com>
2024-05-06 00:57:10 +0200

    [tier-01] DS&DA - MODULE-01/task-05 : Learned concatenation of strings.

---
+++ module-01/05-concatenation-of-strings.py
@@ -0,0 +1,24 @@
+'''
+Using the previously created variables first_name and last_name, perform the concatenation of these variables to form the full name.
+
+Tasks:
+Using the variables first_name and last_name, create a new variable full_name.
+The full_name variable should be the result of concatenating (first_name, a space " ", and last_name).
+Use the + operator to concatenate the strings.
+Expected result:
+The program should create and output the full_name variable, which contains the full name composed of the first name and last name, separated by a space.
+
+Tips:
+Make sure there is a space between the first name and last name.
+Remember that the + operator is used to concatenate strings.
+'''
+
+# Declare variables for first name and last name
+first_name = "Viktoriia"
+last_name = "Kratser"
+
+# Concatenate first name and last name with a space in between to form full name
+full_name = first_name + " " + last_name
+
+# Print the full name
+print("Full Name:", full_name)
