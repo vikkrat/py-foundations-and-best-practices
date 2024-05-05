a20e59e0f618273fdc2d0fab50b47cd251474d64
Viktoriia Kratser <poshta.victoria@gmail.com>
2024-05-06 01:13:20 +0200

    [tier-01] DS&DA - MODULE-01/task-08 : Learned data types - summry; Clean-up code.

---
+++ module-01/08-data-types-summary.py
@@ -0,0 +1,30 @@
+'''
+Create four variables of different data types in Python with corresponding values.
+
+Tasks:
+Declare a variable name as a string and assign it a textual value.
+Declare a numerical variable age and assign it an integer value.
+Declare a Boolean variable is_active and assign it one of the Boolean values True or False.
+Declare a variable subscription and set its value to None to indicate the absence of value.
+Expected result:
+After executing the program, you should have four variables: name, age, is_active, subscription, 
+each representing a different data type.
+'''
+
+# Declare a string variable 'name' and assign a textual value
+name = "Viktoriia Kratser"
+
+# Declare an integer variable 'age' and assign an integer value
+age = 34
+
+# Declare a Boolean variable 'is_active' and assign a Boolean value
+is_active = True
+
+# Declare a variable 'subscription' and assign None to indicate absence of value
+subscription = None
+
+# Print the values and types of each variable to verify their types and contents
+print("Name:", name, "Type:", type(name))
+print("Age:", age, "Type:", type(age))
+print("Active:", is_active, "Type:", type(is_active))
+print("Subscription:", subscription, "Type:", type(subscription))
