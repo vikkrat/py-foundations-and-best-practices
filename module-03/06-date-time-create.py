99ad341543bd009bfe21531a2d9562cd3358d275
Viktoriia Kratser <poshta.victoria@gmail.com>
2024-05-06 13:58:53 +0200

    [tier-01] DS&DA - MODULE-03/task-06 : Datetime Create.

---
+++ module-03/06-date-time-create.py
@@ -0,0 +1,11 @@
+'''
+Create a datetime object  for January 7, 2020, time (07:30:50)
+'''
+
+from datetime import datetime
+
+# Create a datetime object for January 7, 2020 (07:30:50)
+specific_datetime = datetime(year=2020, month=1, day=7, hour=7, minute=30, second=50)
+
+# Print the resulting datetime object
+print(specific_datetime)
