58cf487bef29b636ddb69bccef40d1346230b3eb
Viktoriia Kratser <poshta.victoria@gmail.com>
2024-05-07 00:50:23 +0200

    [tier-01] DS&DA - MODULE-07/task-04 : Console Bot Assistant - Added models: commands, messages, errors.

---
+++ module-07/goit-pycore-hw-04/04-console-bot-assistant/models/commands.py
@@ -0,0 +1,11 @@
+from enum import Enum, auto
+
+# This enum holds exeptable for bot-assistant commands.
+class Commands(Enum):
+    HELLO = auto()
+    ADD = auto()
+    CHANGE = auto()
+    PHONE = auto()
+    ALL = auto()
+    CLOSE = auto()
+    EXIT = auto()
